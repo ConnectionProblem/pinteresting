355140d5ed68b5c5b915c77103ee20fe7b5de554
Gilbert Blanchette <gblanchette@gmail.com>
2014-06-06 23:04:44 -0500

    Fix Registration Issues

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,66 +1,42 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update, :destroy]
-  before_filter :authenticate_user!, except:[:index, :show ]
+  before_action :authenticate_user!, except: [:index, :show]
 
-  # GET /pins
-  # GET /pins.json
   def index
-    @pins = Pin.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 3)
+    @pins = Pin.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 8)
   end
 
-  # GET /pins/1
-  # GET /pins/1.json
   def show
   end
 
-  # GET /pins/new
   def new
     @pin = current_user.pins.build
   end
 
-  # GET /pins/1/edit
   def edit
   end
 
-  # POST /pins
-  # POST /pins.json
   def create
     @pin = current_user.pins.build(pin_params)
-
-    respond_to do |format|
-      if @pin.save
-        format.html { redirect_to @pin, notice: 'Pin was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @pin }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @pin.errors, status: :unprocessable_entity }
-      end
+    if @pin.save
+      redirect_to @pin, notice: 'Pin was successfully created.'
+    else
+      render action: 'new'
     end
   end
 
-  # PATCH/PUT /pins/1
-  # PATCH/PUT /pins/1.json
   def update
-    respond_to do |format|
-      if @pin.update(pin_params)
-        format.html { redirect_to @pin, notice: 'Pin was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @pin.errors, status: :unprocessable_entity }
-      end
+    if @pin.update(pin_params)
+      redirect_to @pin, notice: 'Pin was successfully updated.'
+    else
+      render action: 'edit'
     end
   end
 
-  # DELETE /pins/1
-  # DELETE /pins/1.json
   def destroy
     @pin.destroy
-    respond_to do |format|
-      format.html { redirect_to pins_url }
-      format.json { head :no_content }
-    end
+    redirect_to pins_url
   end
 
   private
@@ -71,9 +47,9 @@ class PinsController < ApplicationController
 
     def correct_user
       @pin = current_user.pins.find_by(id: params[:id])
-
-      redirect_to pins_path, notice: "You are not authorized to edit this pin" if @pin.nil?
+      redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
     end
+
     # Never trust parameters from the scary internet, only allow the white list through.
     def pin_params
       params.require(:pin).permit(:description, :image)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ActiveRecord::Base
-	has_many :pins
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  has_many :pins
+
+  validates :name, presence: true
 end
